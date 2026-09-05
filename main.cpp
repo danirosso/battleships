fb888f918c5ecb5745f676cb4964d1e4df0bca69
rosso <daniel.7463138@edu.univali.br>
2026-09-05 04:36:27 -0300

    got got the cursor going bois

--- main.cpp
+++ main.cpp
@@ -13,31 +13,43 @@ int mainMenu();
 //Returns a number which represents the type of game that will be played
 void setUpBoard(int line, int col, int gameBoard[BLINE][BCOL]);
 //Gets the values on each tile of the gameBoard
-Rectangle cursorSetUp (int startX, int startY, int step, int size);
-//Draws the cursor and moves it around, returns the coordinates chosen.
-void printBoard (int gameStyle);
+void cursorSetUp (int startX, int startY, int step, int size, Rectangle *cursor);
+//Initializes the cursor
+void printBoard (int gamePhase, Rectangle visualBoard[BLINE][BCOL], int line, int col, int step, int sqSize);
 //Prints the board differently depending on the state of the game
-
-
+void moveCursor (int startX, int startY, int step, int size, Rectangle *cursor);
+//Moves the position of the cursor around, returns where enter was pressed;
 
 int main (){
 
     InitWindow(800, 600, "Batalha");
     SetTargetFPS(60);
 
-    int gameStyle = 0; 
+    int gamePhase = 0; 
+
     int p1gameBoard[BLINE][BCOL];
     Rectangle visualBoard[BLINE][BCOL];
+    const int step = 25;
+    const int sqSize = 20;
+
+    bool teste = false;
+    Rectangle cursor;
+    if(!teste) cursorSetUp(sqSize, sqSize, step, sqSize, &cursor);
 
     while(!WindowShouldClose()){
         BeginDrawing();
+
+        if(gamePhase == 0){ 
+            gamePhase = mainMenu();
+        }
+
         ClearBackground(BLACK);
 
-        if (gameStyle == 0){ 
-            gameStyle = mainMenu();
+        if(gamePhase == 1){ 
+            printBoard(gamePhase, visualBoard, BLINE, BCOL, step, sqSize);
+            moveCursor(20, 20, step, sqSize, &cursor);
+            DrawRectangleRec(cursor,GREEN);
         }
-        
-        PrintBoard(1);
 
         EndDrawing(); 
     }
@@ -45,16 +57,14 @@ int main (){
     CloseWindow(); 
 }
 
-void printBoard (int gameStyle, int gameBoard, int visualBoard){
-    const int step = 25;
-    const int sqSize = 20;
+void printBoard (int gamePhase, Rectangle visualBoard[BLINE][BCOL], int line, int col, int step, int sqSize){
 
     for (int i = 0; i < line; i++){    /* First we'll initialize the postition */
         for (int j = 0; j < col; j++){ /* of every square, than we'll draw them */
             visualBoard[i][j].x = sqSize + step*i; 
             visualBoard[i][j].y = sqSize + step*j;
             visualBoard[i][j].width = sqSize;
-            visualBoard[i][j].height = sqSize;
+            visualBoard[i][j].height = visualBoard[i][j].width;
         }
     }
 
@@ -63,47 +73,32 @@ void printBoard (int gameStyle, int gameBoard, int visualBoard){
             DrawRectangleRec(visualBoard[i][j], GRAY);
         }
     }
-
-
-bool setUpBoard(int line, int col, int gameBoard[BLINE][BCOL]){
-
-    double frame = GetFrameTime();
-    if (frame > 1.0f) {
-        Rectangle answer = cursorSetUp(visualBoard[0][0].x, visualBoard[0][0].y, step, sqSize);
-        DrawRectangleRec(answer, YELLOW);
-        frame = 0;
-    }
 }
-
-Rectangle cursorSetUp (int startX, int startY, int step, int size){
-    Rectangle cursor;
-    bool isDefined = false;
-
-    while(!isDefined){
-        cursor.x = startX;   /*Initialize a cursor rectangle in the first square*/
-        cursor.y = startY;
-        cursor.width = size;
-        cursor.height = cursor.width;
-        isDefined = true;
-    }
-
-    DrawRectangleRec(cursor, RED);
-
-    Rectangle cursorOld = cursor;
-
-    switch (GetKeyPressed()){
-        case KEY_H: /*if (cursor.x != startX)*/ cursor.x += step; break;
-        case KEY_J: /*if (cursor.y != startY)*/ cursor.y += step; break;
-        case KEY_K: /*if (cursor.x != startX)*/ cursor.x -= step; break;
-        case KEY_L: /*if (cursor.y != startY)*/ cursor.y -= step; break;
-        case KEY_ENTER: return cursor;
-    }
-    if (cursorOld.x != cursor.x || cursor.y != cursorOld.y)
-        DrawRectangleRec(cursorOld, GRAY);
-    DrawRectangleRec(cursor, RED);
-    return cursor;
+ 
+void cursorSetUp (int startX, int startY, int step, int size, Rectangle *cursor){
+    cursor->x = startX;
+    cursor->y = startY;
+    cursor->width = size;
+    cursor->height = cursor->width;
 }
+ 
+void moveCursor (int startX, int startY, const int step, const int size, Rectangle *cursor){
+    /*NGL, this took a while. */
 
+    Rectangle cursorOld = *cursor; 
+
+    if (IsKeyPressed(KEY_UP)) cursor->y -= step;
+    if (IsKeyPressed(KEY_DOWN)) cursor->y += step;
+    if (IsKeyPressed(KEY_LEFT)) cursor->x -= step;
+    if (IsKeyPressed(KEY_RIGHT)) cursor->x += step;
+
+    if (cursor->x < startX || cursor->x > startX + step * (BCOL-1)) cursor->x = cursorOld.x;
+    if (cursor->y < startY || cursor->y > startY + step * (BLINE-1)) cursor->y = cursorOld.y;
+     
+    //if (cursorOld.x != cursor->x || cursor->y != cursorOld.y){
+    //    DrawRectangleRec(cursorOld, RED);
+    //}
+}
 
 int mainMenu(){
 
@@ -114,10 +109,8 @@ int mainMenu(){
         DrawText("Start playing!", 400, 20, 20, LIGHTGRAY);
 
         if (GuiButton((Rectangle){400, 200, 60, 20}, "1 player") || IsKeyDown(KEY_ENTER)){
-            DrawText("Game start!", 600, 20, 40, RED);
             chosenGameStyle = 1; 
             gameStarted = true;
-            ClearBackground(BLACK);
         }
 
         if (GuiButton((Rectangle){400, 550, 60, 20}, "Quit") || IsKeyDown(KEY_Q)){
